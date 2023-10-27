9e644b5408addebaabecfc0943b2758c82c30bb1
Jan Michalski <jan.michalski@intel.com>
2023-10-27 07:45:21 -0400

    common: introduce ruler.py
    This tool is meant to list stack size requirements when given a call stack.
    
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

---
+++ src/ruler.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import re
+import json
+
+STACK_USAGE_FILE='funcs.json'
+CALL_STACK_FILE='call_stack.json'
+
+def dump(var, name):
+        with open(f"{name}.json", "w") as outfile: 
+                json.dump(var, outfile, indent = 4)
+
+def load_stack_usage():
+        with open(STACK_USAGE_FILE, 'r') as file:
+                return json.load(file)
+
+def load_call_stack():
+        with open(CALL_STACK_FILE, 'r') as file:
+                return json.load(file)
+
+def main():
+        funcs = load_stack_usage()
+        call_stack = load_call_stack()
+        for func in call_stack['stack']:
+                if func in funcs.keys():
+                        size = funcs[func]['size']
+                else:
+                        size = 0
+                print(f'{size} {func}')
+
+if __name__ == '__main__':
+        main()
